254a01941a98d946fed1bc7ab09d15749f30c78d
snowingfox <1503401882@qq.com>
2024-06-14 21:33:17 +0800

    chore: change url

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,7 +10,7 @@ export class AppController {
     return createResponse('获取最新版本号成功', {
       latest_version: '1.0.1',
       latest_url:
-        'https://internal-api-drive-stream.feishu.cn/space/api/box/stream/download/all/JNGjbxYFyoswqExUQiBccXKsnxh',
+        'https://xq261aa61x.feishu.cn/file/IK5fbSt2uoleq2xS5NOc1UKunIh?from=from_copylink',
     })
   }
 }
